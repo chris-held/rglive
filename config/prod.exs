550ccaf8addd7339318c27011226d6922547dfe3
Chris Held <cheld100@gmail.com>
2020-05-22 14:20:54 -0500

    prod deploy config

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,20 @@ config :rglive, RgliveWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :rg_live, RgliveWeb.Endpoint,
+  # Possibly not needed, but doesn't hurt
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :rg_live, RgliveWeb.Endpoint.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+  pool_size: 2
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
