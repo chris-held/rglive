515582b260963f5591212482cc567e3a89d4f003
Chris Held <cheld100@gmail.com>
2020-05-22 14:34:59 -0500

    heroku config changes

--- config/prod.exs
+++ config/prod.exs
@@ -10,25 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rglive, RgliveWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "cryptic-castle-04625.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-config :rg_live, RgliveWeb.Endpoint,
-  # Possibly not needed, but doesn't hurt
-  http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  server: true
-
-config :rg_live, RgliveWeb.Endpoint.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  ssl: true,
-  # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
-  pool_size: 2
 
 # ## SSL Support
 #
